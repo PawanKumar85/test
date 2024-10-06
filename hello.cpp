2c9b1b1f2db6bb0d5e87bb2c1cc28c79486a01c4
Pawan Kumar <pawan630703@gmail.com>
2024-10-07 02:52:29 +0530

    hello.cpp file is added only

--- hello.cpp
+++ hello.cpp
@@ -10,4 +10,22 @@ int main()
     cout << a;
 }
 
+// first time when you create new repository
+git init 
+git add .
+git commit -m "your message"
+git branch -M main
+git remote add origin "url"
+git push -u origin main
+
+
+// after that you can use this
+git add .
+git commit -m "your message"
+git push
+
+
+// git add .
+. -> select all
+hello.c  -> git add hello.c  git add "file ka nam"
 
